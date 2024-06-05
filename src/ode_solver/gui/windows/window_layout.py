ff275655dbf4141abad74f8987fb1030b8d34374
Dave Walker <david.walker5@icloud.com>
2024-06-05 16:12:48 +0100

    Dependabot updates

--- src/ode_solver/gui/windows/window_layout.py
+++ src/ode_solver/gui/windows/window_layout.py
@@ -49,7 +49,7 @@ def create_main_window(menu_definition):
     ]
 
     # Create and return the window
-    window = sg.Window("ODE Solver v1.1.2",
+    window = sg.Window("ODE Solver v1.2.0",
                        layout,
                        resizable=True,
                        margins=(0, 0),
