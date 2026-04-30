651a4eb8a40346a0c3fb53c651e7ff5a1f14df7e
Dave Walker <david.walker5@icloud.com>
2026-04-30 13:35:42 +0100

    Properly format invalid option errors

--- src/ode_solver/cli/ode_solver_cli.py
+++ src/ode_solver/cli/ode_solver_cli.py
@@ -7,7 +7,7 @@ from ode_solver.utils.data_exchange import write_simulation
 from ode_solver.utils.chart import export_chart
 
 
-def show_argument_error(message):
+def print_error(message):
     """
     Show an argument processing error
 
@@ -26,28 +26,28 @@ def handle_args(args):
 
     # Load the simulation file
     if not args.simulation:
-            show_argument_error(f"A simulation file is required to run the ODE Solver from the command line")
+            print_error(f"A simulation file is required to run the ODE Solver from the command line")
             return False
 
     if not load_simulation_from_args(args.simulation):
-        show_argument_error(f"Missing or invalid simulation file:\n{args.simulation}\n")
+        print_error(f"Missing or invalid simulation file:\n{args.simulation}\n")
         return False
 
     # Handle the export flag
     if args.export:
         if not args.simulation:
-            show_argument_error(f"A simulation file must be specified to use export")
+            print_error(f"A simulation file must be specified to use export")
             return False
 
         # Check export to a supported type has been requested
         if not check_export_format(args.export):
-            show_argument_error(f"Invalid export format specified")
+            print_error(f"Invalid export format specified")
             return False
 
     # Handle the charting flag
     if args.chart:
         if not args.simulation:
-            show_argument_error(f"A simulation file must be specified to export a chart")
+            print_error(f"A simulation file must be specified to export a chart")
             return False
 
     return True
@@ -67,9 +67,9 @@ def cli_main(args):
     # Validate the options
     simulation_options, invalid_options = pre_run_validate_options()
     if len(invalid_options) > 0:
-        from pprint import pprint as pp
-        pp(invalid_options)
-        return
+        for o in invalid_options:
+            print_error(f"Invalid value for '{o['display_name']}' : '{o['value']}'")
+            return
 
     # Run the solution
     solution_runner = SolutionRunner()
